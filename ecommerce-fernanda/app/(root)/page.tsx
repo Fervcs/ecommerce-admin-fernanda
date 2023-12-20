b0a77b18701d02f011b4d23ce7a10634b5257462
fer <a00837146@tec.mx>
2023-12-20 16:19:53 -0600

    me quede hasta el min 1:05:00 del video, agregue librerías como modals, useEffect useState, añadir la carpeta providers y hooks

--- ecommerce-fernanda/app/(root)/page.tsx
+++ ecommerce-fernanda/app/(root)/page.tsx
@@ -1,11 +1,24 @@
-import Image from 'next/image'
-import { Button } from "@/components/ui/button"
+"use client"
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 
-export default function Home() {
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+  if (!isOpen){
+    onOpen(); 
+  }
+}, [isOpen, onOpen]);
+
   return (
-    <div>
-        <Button variant="outline">Button</Button>
-    </div>
-  )
+   <div className='p-4'>
+    Root Page
+   </div>
+  );
 }
+
+export default SetupPage;
